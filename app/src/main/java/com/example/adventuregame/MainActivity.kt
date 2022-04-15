ffd81669b096d356f9c9236e10c1ad49b18c9d59
imadsharof <imadsharof@hotmail.com>
2022-04-16 00:15:13 +0200

    premier commit

--- app/src/main/java/com/example/adventuregame/MainActivity.kt
+++ app/src/main/java/com/example/adventuregame/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.adventuregame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 /*ceci est un commentaire
-*
-* hohohoh*/
+*/
+/*test commentaire*/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
