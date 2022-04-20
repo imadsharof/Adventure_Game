a2d76f2164aa50d53f0d118e5fade0bc331488ee
zakaria <zakariaz@hotmail.be>
2022-04-20 17:49:00 +0200

    projet

--- app/src/main/java/com/example/adventuregame/Personnage.kt
+++ app/src/main/java/com/example/adventuregame/Personnage.kt
@@ -13,4 +13,14 @@ class Personnage (x1: Float, y1: Float, x2: Float, y2: Float) {
     val paint = Paint()
     var dx = 1
     var dy = 0
+
+    fun saute() {
+        dy = 2
+        r.offset(3.0F*dx, 3.0F*dy)
+    }
+
+    fun gauche() {
+        dx = - dx
+        r.offset(3.0F*dx, 3.0F*dy)
+    }
 }
