9f8cde753fd297488badd63be7978235a9f7f479
Opklpo <Azertyuiop@gmail.com>
2022-04-21 14:08:43 +0200

    Salut

--- app/src/main/java/com/example/adventuregame/Petits monstres.kt
+++ app/src/main/java/com/example/adventuregame/Petits monstres.kt
@@ -1,4 +1,34 @@
 package com.example.adventuregame
 
-class Petitsmonstres {
+import android.graphics.Bitmap
+import androidx.core.graphics.createBitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import java.util.*
+class Petitsmonstres(x: Float, y: Float, x: Float, y: Float) {
+    val paint = Paint()
+    val speed = 0.3f
+    val size(0.5f, 0.5f)
+    val attackRange = 0.15f
+    val attackCooldown = 2f
+    val damage = 2f
+    val life = 10
+    val detectRange = 2.5f
+    }
+
+    fun draw (canvas: Canvas) { /* Dessin du personnage représenté par un rectangle*/
+        paint.color = color
+        canvas.drawRect(r,paint)
+    }
+
+    fun attack() {
+        if personnage in detectRange
+
+
+
+
+
+
 }
