3af7994023c2adbeec5090c78194f2aca8718ebc
imadsharof <imadsharof@hotmail.com>
2022-05-04 18:36:19 +0200

    commit : thread

--- app/src/main/java/com/example/adventuregame/MainActivity.kt
+++ app/src/main/java/com/example/adventuregame/MainActivity.kt
@@ -51,42 +51,31 @@ class MainActivity() : AppCompatActivity(), View.OnTouchListener{
         val touchPoint = Point(e.x.toInt(), e.y.toInt())
 
         when(v.id){
-            droite.id -> {if(e.action ==MotionEvent.ACTION_DOWN ) {
-                resteappuie()
+            droite.id -> {
+                if(e.action ==MotionEvent.ACTION_DOWN ) { var isPressed = true
+                    Thread {
+                        while (isPressed) {
+                            if (drawingView.personnage.x2 > drawingView.screenWidth / 2){val isPressed = false}
+                            drawingView.personnage.droite()
+                            drawingView.personnage.x1 += 10f
+                            drawingView.personnage.x2 += 10f
+                            drawingView.personnage.setRect()
+                            for (m in drawingView.lesmonstres){ /*Si perso touche monstre*/
+                                m.setRect()
+                                if((m.r.left == drawingView.personnage.r.right  && m.r.top < drawingView.personnage.r.top) ){drawingView.life -= 1 }
+                                else if(m.r.top == drawingView.personnage.r.bottom){drawingView.life -= 1}
+                                if (drawingView.life==0){drawingView.personnage.dead = true}
+                            }
+                            Thread.sleep(100)
+                            if (!Thread.interrupted()){var isPressed = false}
+                        }
+
+                    }.start()
             }
             else if(e.action ==MotionEvent.ACTION_UP ) { Thread.interrupted() == true }}
         gauche.id ->{}}
         return true
         }
-
-
-
-    fun resteappuie() {
-        var isPressed = true
-        Thread {
-            while (isPressed) {
-                if (drawingView.personnage.x2 > drawingView.screenWidth / 2){val isPressed = false}
-                drawingView.personnage.droite()
-                drawingView.personnage.x1 += 10f
-                drawingView.personnage.x2 += 10f
-                drawingView.personnage.setRect()
-                for (m in drawingView.lesmonstres){ /*Si perso touche monstre*/
-                    m.setRect()
-                    if((m.r.left == drawingView.personnage.r.right  && m.r.top < drawingView.personnage.r.top) ){drawingView.life -= 1 }
-                    else if(m.r.top == drawingView.personnage.r.bottom){drawingView.life -= 1}
-                    if (drawingView.life==0){drawingView.personnage.dead = true}
-                }
-                Thread.sleep(100)
-                if (!Thread.interrupted()){var isPressed = false}
-                    }
-
-                }.start()
-            }
-
-
-   fun appuieplus() {
-        var isPressed = false
-    }
 
     override fun onPause() {
         super.onPause()
